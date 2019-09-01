0e45ed860c1a1fd704d79a49efe2cb6a730b5984
jurek <jurek@tenar.pl>
2019-09-01 13:34:01 +0200

    webpack

--- react/gulpfile.js
+++ react/gulpfile.js
@@ -1,6 +1,26 @@
 const { src, dest, parallel } = require('gulp');
 const babel = require('gulp-babel');
+const streamify = require('gulp-streamify');
+
+// with browserify:
 const browserify = require('gulp-browserify');
+const uglify = require('gulp-uglify');
+
+// with webpack:
+const webpack = require('webpack');
+const webpackStream = require('webpack-stream');
+
+
+const webpackConfig = { // minimal config
+  entry: './tmp/SimpleComponent.js',
+  output: {
+    filename: 'SimpleComponent.js'
+  },
+  mode: 'production'
+//  mode: 'development'
+};
+
+
 
 const babelOptions = {
     presets: [
@@ -14,15 +34,25 @@ const babelOptions = {
 }
 
 
-function build() {
+function build_b() {
   return src('./src/SimpleComponent.jsx')
     .pipe(babel(babelOptions))
     .pipe(browserify({
           insertGlobals : true,
           debug : true
         }))
-    .pipe(dest('./static/js'));
+    .pipe(streamify(uglify()))
+    .pipe(dest('./static/js'))
+}
+
+function build() {
+  return src('./src/SimpleComponent.jsx')
+    .pipe(babel(babelOptions)).pipe(dest('./tmp'))
+    .pipe(webpackStream(webpackConfig), webpack)
+    .pipe(dest('./static/js'))
 }
 
 
+
 exports.build=build;
+
